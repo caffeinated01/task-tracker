0cee5ca2c17212d698a60c4d28663d50ba8ac308
Javier <javier.has.no.life@gmail.com>
2026-04-21 21:33:27 +0800

    write backend for board

--- app/api/board.py
+++ app/api/board.py
@@ -1,20 +1,49 @@
 from flask import Blueprint, g, jsonify
 
-from app.utils.decorators import access_token_required
+from app.db import get_db
+from app.utils.decorators import access_token_required, json_required
+from app.crud.board import create_board, get_boards_for_user, get_board_by_id_for_user
 
 bp = Blueprint('board', __name__, url_prefix='/api/board')
 
 
-@bp.route("/fetch", methods=["GET"])
+@bp.route("/boards", methods=["GET"])
 @access_token_required
 def fetch_boards():
-    return jsonify({"boards": ["1", "2", "3"]})  # placeholder
+    db = get_db()
+    user_id = g.current_user["id"]
+
+    boards = get_boards_for_user(db, user_id)
+    return jsonify(boards)
 
 
 @bp.route("/create", methods=["POST"])
 @access_token_required
-def create_board():
-    return
+@json_required
+def create_board(data):
+    user_id = g.current_user["id"]
+    board_name = data.get("name")
+
+    if not board_name:
+        return jsonify({"message": "Missing name"}), 400
+
+    db = get_db()
+    board_id = create_board(db, board_name, user_id)
+    return jsonify({"id": board_id, "name": board_name}), 201
+
+
+@bp.route("/board/<int:id>", methods=["GET"])
+@access_token_required
+def fetch_board(id):
+    user_id = g.current_user["id"]
+
+    db = get_db()
+    board = get_board_by_id_for_user(db, id, user_id)
+
+    if not board:
+        return jsonify({"message": "Board not found"}), 404
+    
+    return jsonify(board)
 
 
 @bp.route("/update", methods=["POST"])
---
+++ app/crud/board.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+def create_board(db: sqlite3.Connection, name, owner_id):
+    cursor = db.execute(
+        "INSERT INTO boards (name, owner_id) VALUES (?, ?)", (name, owner_id))
+    db.commit()
+    board_id = cursor.lastrowid  # get auto generated id from insert
+    db.execute("INSERT INTO board_users (user_id, board_id, role) VALUES (?, ?, ?)",
+               (owner_id, board_id, "owner"),)
+    db.commit()
+    return board_id
+
+
+def get_boards_for_user(db: sqlite3.Connection, user_id):
+    boards = db.execute(
+        """
+        SELECT b.id, b.name, bu.role
+        FROM boards b
+        JOIN board_users bu ON b.id = bu.board_id
+        WHERE bu.user_id = ?
+        """, (user_id,),
+    ).fetchall()
+
+    return [dict(board) for board in boards]
+
+
+def get_board_by_id_for_user(db: sqlite3.Connection, board_id, user_id):
+    board = db.execute(
+        """
+        SELECT b.id, b.name, bu.role
+        FROM boards b
+        JOIN board_users bu ON b.id = bu.board_id
+        WHERE b.id = ? AND bu.user_id = ?
+        """,
+        (board_id, user_id),
+    ).fetchone()
+
+    return dict(board) if board else None
