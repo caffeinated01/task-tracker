009c1ab28cfc0ba609a209442742a2eb9414cd8a
Bobby Lee <92657473+fortyonehex@users.noreply.github.com>
2026-04-23 23:14:46 +0800

    Id gen + make templates

--- app/static/board_disp.js
+++ app/static/board_disp.js
@@ -1,14 +1,39 @@
+/*
+handle general clicking on webpage, this should cover:
+*   clicking on dropdown menus (all forms)
+*/
+
+function clickHandle(e) {
+    // https://stackoverflow.com/questions/46732637/best-way-to-handle-clicks-on-buttons-in-javascript-vanilla
+    // do something similar to this for edit, dropdowns and shit
+
+    console.log(e.target.id.contains());
+}
+
+// handle dragging on webpage
+
 let preview = document.querySelector('.hover-preview');
 let previewOffset = {x: null, y: null, originalStatus: null};
+let draggingTask = false;
 
 const emptyImg = document.createElement('img'); 
 emptyImg.src = 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==';
 
-document.addEventListener("dragstart", (e) => {
-    e.target.classList.add('dragging');
+function dragStartHandle(e) {
+    // check if task element is being selected
+    draggingTask = e.target.classList && e.target.classList.contains('task');
+    if (!draggingTask) return;
 
-    var parentStatusBox = document.elementsFromPoint(e.clientX, e.clientY)
-                                  .find((x) => x.classList.contains('flex-status'));
+    // exclude selecting option button or dropdown menu
+    var elementsAtPos = document.elementsFromPoint(e.clientX, e.clientY);
+    var shouldBeIgnored = elementsAtPos.find((x) => x.classList.contains('option-dropdown-comp') || 
+                                                    x.classList.contains('option-dropdown-menu'));
+    if (shouldBeIgnored) {
+        draggingTask = false;
+        return;
+    }
+
+    var parentStatusBox = elementsAtPos.find((x) => x.classList.contains('flex-status'));
     if (parentStatusBox) {
         const statusName = parentStatusBox.querySelector('.status-head > h2').textContent;
         preview.querySelector('h3').textContent = "Set status to \"" + statusName + "\"";
@@ -20,14 +45,17 @@ document.addEventListener("dragstart", (e) => {
     preview.style.height = elementRect.height + 'px';
 
     previewOffset.x = e.layerX;
-    previewOffset.y = e.clientY - elementRect.top;
+    previewOffset.y = e.clientY - elementRect.top;          // hack to make sure y of the offset meets
+                                                            // can't tell if this is intended behaviour
 
     document.body.appendChild(preview);
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setDragImage(emptyImg, 0, 0);
-})
+}
 
-document.addEventListener("dragover", (e) => {
+function dragOverHandle(e) {
+    if (!draggingTask) return;
+
     preview.style.transform = 'translate(' + (e.clientX - previewOffset.x + 9999) + 'px, '
                             + (e.clientY - previewOffset.y + 9999) + 'px)';
 
@@ -48,10 +76,11 @@ document.addEventListener("dragover", (e) => {
 
     e.preventDefault()
     e.dataTransfer.dropEffect = 'move';
-    // console.log(e.target.closest('.flex-status'));
-});
+}
 
-document.addEventListener("dragend", (e) => {
+function dragEndHandle(e) {
+    if (!draggingTask) return;
+
     e.target.classList.remove('dragging');
     var parentStatusBox = document.elementsFromPoint(e.clientX, e.clientY)
                                   .find((x) => x.classList.contains('flex-status'));
@@ -59,10 +88,19 @@ document.addEventListener("dragend", (e) => {
     if (parentStatusBox && !parentStatusBox.querySelector('.status-list').childNodes.values().find((x) => x == e.target)) {
         parentStatusBox.querySelector('.status-list').appendChild(e.target);
     }
-    console.log(e.target);
-    console.log(parentStatusBox);
+    // console.log(e.target);
+    // console.log(parentStatusBox);
 
     preview.style.left = '-9999px';
     preview.style.top = '-9999px';
     preview.style.transform = '';
-});
+
+    // add stuff to update main page
+}
+
+// connect handles
+
+document.addEventListener("dragstart", dragStartHandle);
+document.addEventListener("dragover", dragOverHandle);
+document.addEventListener("dragend", dragEndHandle);
+document.addEventListener("click", clickHandle);
---
+++ app/utils/id_gen.py
@@ -0,0 +1,36 @@
+# Simple implementation of a Linear-feedback shift register to generate pseudo-
+# random numbers. We have a 31-bit number which we cram into 6 base-36 digits
+# (since 6 log_2 36 ~= 31) and use as a unique identifier.
+
+# Values for shift register bits sourced from here:
+# https://www.physics.otago.ac.nz/reports/electronics/ETR2012-1.pdf
+
+import math
+
+lfsr_seed = 0b0110111110100110001011110010011
+lfsr = lfsr_seed
+
+def get_next_id():
+    global lfsr
+    
+    # taps: 31, 30, 29, 28
+    bit = (lfsr ^ (lfsr >> 1) ^ (lfsr >> 2) ^ (lfsr >> 3)) & 1
+    lfsr = lfsr >> 1 | bit << 30
+    return lfsr
+
+def conv_id_to_b36(x):
+    b36_str = ""
+    for i in range(math.ceil(math.log(x, 36))):
+        b36_str = "0123456789abcdefghijklmnopqrstuvwxyz"[(x // 36**i) % 36] + b36_str
+    return b36_str.zfill(6)
+
+def get_next_b36_id():
+    return conv_id_to_b36(get_next_id())
+
+# prove it cycles through all
+if __name__ == "__main__":
+    from tqdm import tqdm
+
+    for cycles in tqdm(range(1, 2**31)):
+        get_next_id()
+    print(cycles)
