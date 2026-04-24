5caa2bb3c96fb93b0235876220bf851fb27b2483
Javier <javier.has.no.life@gmail.com>
2026-04-24 18:04:45 +0800

    format id_gen

--- app/utils/id_gen.py
+++ app/utils/id_gen.py
@@ -1,11 +1,13 @@
-# We use a 38-bit representation of the current Unix timestamp (which lasts us 
-# until around year 10000, good enough) with another 8 bits added to the back 
+# We use a 38-bit representation of the current Unix timestamp (which lasts us
+# until around year 10000, good enough) with another 8 bits added to the back
 # to ensure uniqueness (this is generated using a linear-feedback shift regis-
-# ter). Seed is determined using a random byte. This may lead to collisions, 
+# ter). Seed is determined using a random byte. This may lead to collisions,
 # but only if the server is rerun on the same second it is turned off.
 
 # Values for shift register bits sourced from here:
 # https://www.physics.otago.ac.nz/reports/electronics/ETR2012-1.pdf
+
+# Use ONLY get_next_b36_id() to generate IDs
 
 import math
 import datetime
@@ -17,21 +19,24 @@ lfsr = lfsr_seed
 
 def get_next_lfsr():
     global lfsr
-    
+
     # taps: 13, 12, 10, 9
     bit = (lfsr ^ (lfsr >> 1) ^ (lfsr >> 3) ^ (lfsr >> 4)) & 1
     lfsr = lfsr >> 1 | bit << 12
     return lfsr
+
 
 def get_next_id():
     # make it fit within 38 bits
     curr_time = int(datetime.datetime.now().timestamp()) % 2 ** 38
     return (get_next_lfsr() << 38) + curr_time
 
+
 def conv_id_to_b36(x):
     b36_str = ""
     for i in range(math.ceil(math.log(x, 36))):
-        b36_str = "0123456789abcdefghijklmnopqrstuvwxyz"[(x // 36**i) % 36] + b36_str
+        b36_str = "0123456789abcdefghijklmnopqrstuvwxyz"[
+            (x // 36**i) % 36] + b36_str
     return b36_str.zfill(10)
 
 
@@ -46,7 +51,8 @@ if __name__ == "__main__":
     ledger = bytearray(2**13 // 8)
     for cycles in tqdm(range(2**13)):
         get_next_lfsr()
-        if ledger[lfsr//8] & 1<<(lfsr%8):
+
+        if ledger[lfsr//8] & 1 << (lfsr % 8):
             print(lfsr, cycles)
             break
-        ledger[lfsr//8] |= 1<<(lfsr%8)
+        ledger[lfsr//8] |= 1 << (lfsr % 8)
